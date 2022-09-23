55386976d75caa16fad58248d1993c374ac6a36c
Ana Caicedo <ana.caicedo@edgeuno.com>
2022-09-22 21:23:27 -0500

    Create ToDoList by day

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
 app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+let dailyTasks = ["Buy food", "Cook foof", "Eat Food"];
 
 app.get("/", function (req, res) {
   const today = new Date();
   const currentDay = today.toLocaleString("en-us", { weekday: "long" });
 
-  res.render("list", { day: currentDay });
+  res.render("list", { day: currentDay, taskList: dailyTasks });
+});
+
+app.post("/", function (req, res) {
+  const newTask = req.body.newTask;
+  dailyTasks.push(newTask);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
